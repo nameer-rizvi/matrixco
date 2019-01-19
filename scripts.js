450265c1bc6b3a53ba759b75f5c0b1ea358814f6
Nameer Rizvi <nameerrizvi@planet-namek2.local>
2019-01-19 18:34:13 -0500

    Increased max-width to 2000px, added scrolling effect for about image

--- scripts.js
+++ scripts.js
@@ -33,24 +33,25 @@ function closeNav() {
 
 function scrollingImg() {
   // V1
-  // var div = $("#about");
-  // var img = $("#aboutImg");
-  // $(document).scroll(function() {
-  //   var start = div.offset().top;
-  //   var stop = div.offset().top + div.outerHeight() - 1000;
-  //   var y = $(this).scrollTop();
-  //   console.log(start, stop, y);
-  //   if (y > start) {
-  //     img.css({ position: "fixed" });
-  //     if (y > stop) {
-  //       img.css("top", stop - y);
-  //     } else {
-  //       img.css("top", 0);
-  //     }
-  //   } else {
-  //     img.css({ position: "relative" });
-  //   }
-  // });
+  var div = $("#about");
+  var img = $("#aboutImg");
+  // var header = document.getElementsByTagName("header")[0];
+  var header = $("header");
+  $(document).scroll(function() {
+    var start = div.offset().top;
+    var stop = div.offset().top + div.outerHeight(true) - 1000;
+    var y = $(this).scrollTop();
+    if (y > start) {
+      img.css({ position: "fixed" });
+      if (y > stop) {
+        img.css("top", stop - y);
+      } else {
+        img.css("top", 0);
+      }
+    } else {
+      img.css({ position: "relative" });
+    }
+  });
   // V2
   // var img = $("#aboutImg");
   // var div = $("#about");
@@ -58,12 +59,13 @@ function scrollingImg() {
   // $(window).scroll(() => {
   //   var divY = div.offset().top + div.outerHeight();
   //   var scrollTop = $(window).scrollTop();
+  //   console.log("scrollTop: " + scrollTop, "divY: " + divY);
   //   if (scrollTop > imgY) {
   //     if (scrollTop < divY) {
-  // img.css({
-  //   position: "fixed",
-  //   top: 0
-  // });
+  //       img.css({
+  //         position: "fixed",
+  //         top: 0
+  //       });
   //     } else {
   //       img.css({
   //         position: "absolute"
