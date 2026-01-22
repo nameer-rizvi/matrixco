6436f98e084e93508dea82885909059abb49d827
nameer-rizvi <rizvinameer@gmail.com>
2026-01-22 12:05:31 -0500

    Client changes: update sales pitch, underline subheaders, remove new balance, add country flags

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   smoothScrolling();
   scrollingImg();
   openNav();
@@ -6,18 +6,18 @@ $(document).ready(function() {
 });
 
 function smoothScrolling() {
-  $("a").on("click", function(event) {
+  $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top,
         },
         800,
-        function() {
+        function () {
           window.location.hash = hash;
-        }
+        },
       );
     }
   });
@@ -32,12 +32,10 @@ function closeNav() {
 }
 
 function scrollingImg() {
-  // V1
   var div = $("#about");
   var img = $("#aboutImg");
-  // var header = document.getElementsByTagName("header")[0];
   var header = $("header");
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var start = div.offset().top;
     var stop = div.offset().top + div.outerHeight(true) - 1000;
     var y = $(this).scrollTop();
@@ -52,28 +50,4 @@ function scrollingImg() {
       img.css({ position: "relative" });
     }
   });
-  // V2
-  // var img = $("#aboutImg");
-  // var div = $("#about");
-  // var imgY = img.offset().top;
-  // $(window).scroll(() => {
-  //   var divY = div.offset().top + div.outerHeight();
-  //   var scrollTop = $(window).scrollTop();
-  //   console.log("scrollTop: " + scrollTop, "divY: " + divY);
-  //   if (scrollTop > imgY) {
-  //     if (scrollTop < divY) {
-  //       img.css({
-  //         position: "fixed",
-  //         top: 0
-  //       });
-  //     } else {
-  //       img.css({
-  //         position: "absolute"
-  //       });
-  //     }
-  //   } else
-  //     img.css({
-  //       position: "relative"
-  //     });
-  // });
 }
